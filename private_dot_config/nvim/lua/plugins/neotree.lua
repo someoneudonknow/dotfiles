92df6d3b4fa44f6f335693f543a8d67963d57ef3
someoneudonknow <nguyentu550278@gmail.com>
2025-06-17 17:27:12 +0700

    update

--- private_dot_config/nvim/lua/plugins/neotree.lua
+++ private_dot_config/nvim/lua/plugins/neotree.lua
@@ -1,21 +1,21 @@
 return {
- "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  otps = {
-    close_if_last_window = true,
-    filesystem = {
-      filtered_items = {
-        visible = true,
-        hide_dotfiles = false,
-      },
-    },
-    window = {
-      width = 25,
-    },
-  }
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	otps = {
+		close_if_last_window = true,
+		filesystem = {
+			filtered_items = {
+				visible = true,
+				hide_dotfiles = false,
+			},
+		},
+		window = {
+			width = 25,
+		},
+	},
 }
